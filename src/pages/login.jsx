575448bbc32350fb1add40bd14022078f627aaf0
Bima <bimanugraha732@gmail.com>
2022-01-04 13:39:49 +0700

    Update login.jsx
    Update Login layout.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,24 +9,28 @@ export default function Login() {
                 <div className='col-md-5 p-0'>
                     <img src={login_img} className='img-fluid' alt="vaccine-login" />
                 </div>
-                <div className='col-md-6 my-auto p-5 card shadow-sm'>
-                    <h3 className='text-center mb-3 fw-bold'>MASUK</h3>
-                    <form>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Masukan email'/>
-                        </div>
-                        <div class="mb-1">
-                            <label for="exampleInputPassword1" class="form-label">Kata sandi</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder='Masukan kata sandi'/>
-                        </div>
-                        <h6 className='mb-3'>Lupa kata sandi?</h6>
-                        <div className='text-center'>
-                        <button type="submit" class="btn btn-primary text-uppercase">Masuk</button>
-                        </div>
-                        
-                    </form>
+                <div className='col-md-5 my-auto mx-auto'>
+                    <div className='mx-5 px-5 py-3 card shadow-sm'>
+                        <h3 className='text-center mb-3 fw-bold'>MASUK</h3>
+                        <form>
+                            <div className="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Masukan email'/>
+                            </div>
+                            <div class="mb-1">
+                                <label for="exampleInputPassword1" class="form-label">Kata sandi</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder='Masukan kata sandi'/>
+                            </div>
+                            <a href='#' className='mb-3'>Lupa kata sandi?</a>
+                            <div className='text-center'>
+                                <button type="submit" class="btn btn-primary text-uppercase">Masuk</button>
+                            </div>       
+                        </form>   
+                    </div>
+                    <h6 className='text-center m-5'>Belum punya akun? <a className='stretched-link'>Buat akun</a></h6>
                 </div>
+                
+                
             </div>
             
 
