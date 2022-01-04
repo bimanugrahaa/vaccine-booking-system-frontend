cdff18017cb0f9436af7f8bc4e22d2626fee79ff
Bima <bimanugraha732@gmail.com>
2022-01-04 22:03:06 +0700

    Update register page
    Update register page validation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js'
 import Header from './components/header';
 import Login from './pages/login';
 import Register from './pages/register';
