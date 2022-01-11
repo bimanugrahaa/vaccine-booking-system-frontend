0380547386c7ebcdaf00e1cb2c6afb3301755102
Bima <bimanugraha732@gmail.com>
2022-01-11 13:32:14 +0700

    Update homepage page
    Update homepage page and hero component.

--- src/users/components/hero.jsx
+++ src/users/components/hero.jsx
@@ -6,9 +6,9 @@ export default function Hero() {
         <>
         <div class="hero position-relative overflow-hidden p-3 p-md-5 text-center bg-light">
             <div class="col-md-5 p-lg-5 mx-auto my-5">
-                <h1 class="display-4 fw-bold text-light hero-text">Sayangi dirimu</h1>
-                <h1 class="display-4 fw-bold text-light hero-text mb-5">Sayangi sekitarmu</h1>
-                <button class="btn btn-primary text-uppercase" href="#">Daftar vaksinasi sekarang!</button>
+                <h1 class="fw-bold text-light hero-text">Sayangi dirimu</h1>
+                <h1 class="fw-bold text-light hero-text">Sayangi sekitarmu</h1>
+                <button class="btn btn-primary text-uppercase m-5" href="#">Daftar vaksinasi sekarang!</button>
             </div>
             <div class="product-device shadow-sm d-none d-md-block"></div>
             <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
--- src/users/pages/homepage.jsx
+++ src/users/pages/homepage.jsx
@@ -1,5 +1,6 @@
 import Header from '../components/header';
 import Hero from '../components/hero';
+import about from '../../assets/about-img.png'
 
 export default function Homepage() {
     
@@ -7,6 +8,31 @@ export default function Homepage() {
         <>
         <Header/>
         <Hero/>
+        <div className='card shadow bg-light bg-gradient border-0 text-center p-3 m-5'>
+            <h4 className='text-uppercase fw-bold mb-3'>Cari fasilitas kesehatan!</h4>
+            <input className='form-control' type="text" placeholder='Rumah sakit ceria'/>
+            <button className='btn btn-primary text-uppercase m-auto mt-3'>Cari</button>
+        </div>
+        <h6 className='ms-5 fw-bold'>Tentang</h6>
+        <div className='row'>
+            <div className='col-lg-1'></div>
+            <div className='col-lg-3'>
+                <p>AyoVaksin! merupakan sebuah website yang berguna untuk melakukan booking vaksin. 
+                    AyoVaksin! dapat melakukan pencarian fasilitas kesehatan, pengecekan vaksin tiap 
+                    fasilitas kesehatan, hingga melakukan pendaftaran vaksin secara mudah. Yuk daftar 
+                    melalui AyoVaksin!
+                </p>
+            </div>
+            <div className='col-lg-4'></div>
+            <div className='col-lg-auto'>
+                <img className='shadow rounded' src={about} alt="" />
+            </div>
+        </div>
+        <footer class="footer mt-5 py-3 bg-light">
+            <div class="container">
+                <span class="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
         </>
     )
 }
