170e44b16b33e2d4b1ac44acef68cc1c9fb2a885
Bima <bimanugraha732@gmail.com>
2022-01-12 16:10:41 +0700

    Update login.jsx
    Integrate login with backend.

--- src/users/pages/login.jsx
+++ src/users/pages/login.jsx
@@ -1,7 +1,45 @@
+import axios from 'axios'
+import { useState } from 'react/cjs/react.development'
 import login_img from '../../assets/vaccine-one.png'
 
 export default function Login() {
     
+    /* Base data login for user */
+    const baseUserLogin = {
+        email: "",
+        password: ""
+    }
+
+    /* useState for Login user */
+    const [user, setUser] = useState(baseUserLogin)
+    const [getError, setError] = useState("")
+
+    /* Login function => validate data to server */
+    const login = async (e) => {
+        e.preventDefault()
+
+        var config = {
+            method: 'post',
+            url: 'http://localhost:8000/user/login',
+            data : user
+        };
+
+        await axios(config)
+            .then(response => {
+                console.log(response)
+            })
+            .catch((error) => {
+
+                const status = error.response.status;
+
+                if (status === 500) {
+                    setError("Data yang Anda masukan salah!")
+                }
+
+                console.error('There was an error!', error);
+            });
+    }
+
     return (
         <>
         <div className='container-fluid'>
@@ -12,15 +50,18 @@ export default function Login() {
                 <div className='col-md-5 my-auto mx-auto'>
                     <div className='mx-5 px-5 py-3 card shadow-sm'>
                         <h3 className='text-center mb-3 fw-bold'>MASUK</h3>
-                        <form>
+                        <form onSubmit={login}>
                             <div className="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Masukan email'/>
+                                <input name='email' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Masukan email'
+                                value={user.email} onChange={(e) => setUser({...user, [e.target.name]: e.target.value})}/>
                             </div>
                             <div class="mb-1">
                                 <label for="exampleInputPassword1" class="form-label">Kata sandi</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder='Masukan kata sandi'/>
+                                <input name='password' type="password" class="form-control" id="exampleInputPassword1" placeholder='Masukan kata sandi'
+                                value={user.password} onChange={(e) => setUser({...user, [e.target.name]: e.target.value})}/>
                             </div>
+                            <small className="text-danger">{getError}</small><br/>
                             <a href='#' className='mb-3'>Lupa kata sandi?</a>
                             <div className='text-center'>
                                 <button type="submit" class="btn btn-primary text-uppercase">Masuk</button>
